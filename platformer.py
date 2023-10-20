e9874baa9d9094510d39bce03cbb869ac9c70a99
zacaffinito <142935239+zacaffinito@users.noreply.github.com>
2023-10-20 13:37:28 -0700

    Add files via upload

---
+++ platformer.py
@@ -0,0 +1,201 @@
+# Created by: Zac Affinito
+
+# import libraries and modules
+import pygame as pg
+from pygame.sprite import Sprite
+import random
+from random import randint
+import os
+
+vec = pg.math.Vector2
+
+# setup asset folders here - images sounds etc.
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'images')
+
+# game settings 
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+SCORE = 0
+
+# player settings
+PLAYER_JUMP = 30
+PLAYER_GRAV = 1.5
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+
+class Player(Sprite):
+    # this is the init method where prperties can be setup for a class
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50, 50))
+        self.image.fill(GREEN)
+        # use an image for player sprite...
+        self.image = pg.image.load(os.path.join(img_folder, 'theBell.png')).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        print(self.rect.center)
+    def controls(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -5
+        if keys[pg.K_d]:
+            self.acc.x = 5
+        if keys[pg.K_SPACE]:
+            self.jump()
+    def jump(self):
+        hits = pg.sprite.spritecollide(self, all_platforms, False)
+        if hits:
+            print("i can jump")
+            self.vel.y = -PLAYER_JUMP
+    def update(self):
+        # self.rect.x += 5
+        # self.rect.y += 5
+        self.acc = vec(0,PLAYER_GRAV)
+        self.controls()
+        # hits = pg.sprite.spritecollide(self, all_platforms, False)
+        # if hits:
+        #     print("i've collided...") 
+        print(all_platforms)
+        # if friction - apply here
+        self.acc.x += self.vel.x * -0.2
+        self.acc.y += self.vel.y * -0.2
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        if self.rect.x > WIDTH:
+            self.rect.x = 0
+        if self.rect.y > HEIGHT:
+            self.rect.y = 0
+        self.rect.midbottom = self.pos
+
+# platforms
+
+class Platform(Sprite):
+    def __init__(self, x, y, w, h):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        print(self.rect.center)
+
+class Mob(Sprite):
+
+    def _init_(self, x, y, w, h, category):
+
+        Sprite._init_(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.category = category
+
+    def update(self):
+
+        if self.category == "moving":
+
+            self.rect.y += self.speed
+            self.rect.x + self.rect.w > WIDTH or self.rect.x < 0
+            self.speed = -self.speed
+            self.rect.y += 25
+
+
+all_sprites = pg.sprite.Group()
+all_platforms = pg.sprite.Group()
+all_mobs = pg.sprite.Group()
+
+for i in range(0,20):
+    m = Mob(randint(0,WIDTH), randint(0,HEIGHT), 25, 25, "moving")
+    all_sprites.add(m)
+    all_mobs.add(m)
+
+# init pygame and create a window
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("My Game...")
+clock = pg.time.Clock()
+
+# create a group for all sprites
+all_platforms = pg.sprite.Group()
+
+# instantiate classes
+player = Player()
+plat = Platform(150, 300, 100, 30)
+plat1 = Platform(200, 200, 100, 30)
+
+# add instances to groups
+all_sprites.add(player)
+
+for plat in PLATFORM_LIST:
+    p = Platform(*plat)
+    all_sprites.add(p)
+    all_platforms.app(p)
+
+
+
+# Game loop
+running = True
+while running:
+    # keep the loop running using clock
+    clock.tick(FPS)
+        
+    for event in pg.event.get():
+        # check for closed window
+        if event.type == pg.QUIT:
+            running = False
+    
+    ############ Update ##############
+    # update all sprites
+    all_sprites.update()
+
+    # this is what prevents the player from falling through the platform when falling down...
+    if player.vel.y > 0:
+            hits = pg.sprite.spritecollide(player, all_platforms, False)
+            if hits:
+                player.pos.y = hits[0].rect.top
+                player.vel.y = 0
+                
+    # this prevents the player from jumping up through a platform
+    if player.vel.y < 0:
+        hits = pg.sprite.spritecollide(player, all_platforms, False)
+        if hits:
+            print("ouch")
+            SCORE -= 1
+            if player.rect.bottom >= hits[0].rect.top - 5:
+                player.rect.top = hits[0].rect.bottom
+                player.acc.y = 5
+                player.vel.y = 0
+
+    ############ Draw ################
+    # draw the background screen
+    screen.fill(BLACK)
+    # draw all sprites
+    all_sprites.draw(screen)
+    draw_text("Score: " + str(SCORE), 22, WHITE, WIDTH/2, HEIGHT/10)
+
+    # buffer - after drawing everything, flip display
+    pg.display.flip()
+
+pg.quit()
---
+++ settings.py
@@ -0,0 +1,26 @@
+# This file was created by Zac Affinito
+
+# game settings
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+SCORE = 0
+
+# player settings
+PLAYER_JUMP = 30
+PLAYER_GRAV = 1.5
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+SKYBLUE = (150, 200, 255)
+
+# starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20)
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20)
+                 (175, 100, 50, 20)]
